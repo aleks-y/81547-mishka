c1516d6ef2ded70ee22d21f2a997be75ed768a6c
aleks-y <ustin89@mail.ru>
2016-10-01 21:42:43 +0300

    featured adaptive

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-var navMain = document.querySelector(".nav-main");
-var navToggle = document.querySelector(".nav-main__toggle");
+var navMain = document.querySelector('.nav-main');
+var navToggle = document.querySelector('.nav-main__toggle');
 
-navMain.classList.remove("nav-main--no-js");
+navMain.classList.remove('nav-main--no-js');
 
-navToggle.addEventListener("click", function() {
-  if (navMain.classList.contains("nav-main--closed")) {
-    navMain.classList.remove("nav-main--closed");
-    navMain.classList.add("nav-main--opened");
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('nav-main--closed')) {
+    navMain.classList.remove('nav-main--closed');
   } else {
-    navMain.classList.add("nav-main--closed");
-    navMain.classList.remove("nav-main--opened");
+    navMain.classList.add('nav-main--closed');
   }
 });
