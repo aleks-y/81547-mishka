2ebacf63a0c227c59cdfdb9531e30b4c5b4cfbf1
aleks-y <ustin89@mail.ru>
2016-10-05 13:26:57 +0300

    main logo adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,11 @@
 "use strict";
 
 var gulp = require("gulp");
-var plumber = require("gulp-plumber");
-var del = require("del");
-var rename = require("gulp-rename");
 var less = require("gulp-less");
+var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var mqpacker = require("css-mqpacker");
-var minify = require("gulp-csso");
-var imagemin = require("gulp-imagemin");
-var svgmin = require("gulp-svgmin");
-var svgstore = require("gulp-svgstore")
 var server = require("browser-sync").create();
-var run = require("run-sequence");
-
-gulp.task("clean", function() {
-  return del("build");
-});
-
-gulp.task("copy", function() {
-  return gulp.src([
-    "fonts/**/*{.woff,.woff2}",
-    "img/**",
-    "js/**",
-    "*.html"
-    ], {
-      base: "."
-    })
-    .pipe(gulp.dest("build"));
-});
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -42,34 +18,10 @@ gulp.task("style", function() {
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]}),
-      mqpacker({
-        sort: true
-      })
+      ]})
     ]))
     .pipe(gulp.dest("css"))
-    .pipe(minify())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("css"));
-});
-
-gulp.task("images", function() {
-  return gulp.src("img/**/*.{png,jpg,gif}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true})
-    ]))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("symbols", function() {
-  return gulp.src("img/icons/*.svg")
-    .pipe(svgmin())
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename("symbols.svg"))
-    .pipe(gulp.dest("img"));
+    .pipe(server.stream());
 });
 
 gulp.task("serve", ["style"], function() {
@@ -82,15 +34,4 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
-});
-
-gulp.task("build", function(fn) {
-  run(
-      "clean",
-      "copy",
-      "style",
-      "images",
-      "symbols",
-      fn
-  );
 });
