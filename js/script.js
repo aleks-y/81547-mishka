fe11820125cc6071b2637fb31c3b39ec497a3ea1
aleks-y <ustin89@mail.ru>
2016-10-06 11:34:55 +0300

    modal open csript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
-var navMain = document.querySelector('.nav-main');
-var navToggle = document.querySelector('.nav-main__toggle');
+var navMain = document.querySelector(".nav-main");
+var navToggle = document.querySelector(".nav-main__toggle");
+var btnOrderFeature = document.querySelector(".btn--order-feature");
+var modalOrder = document.querySelector(".modal-order");
+var btnMakeOrder = modalOrder.querySelector(".btn--make-order");
 
-navMain.classList.remove('nav-main--no-js');
+function navMobile() {
+  navMain.classList.toggle("nav-main--closed");
+};
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('nav-main--closed')) {
-    navMain.classList.remove('nav-main--closed');
-  } else {
-    navMain.classList.add('nav-main--closed');
+navMain.classList.remove("nav-main--no-js");
+navToggle.addEventListener("click", navMobile);
+
+btnOrderFeature.addEventListener("click", function() {
+  event.preventDefault();
+  modalOrder.classList.remove("modal-order--modal-close");
+});
+
+btnMakeOrder.addEventListener("click", function() {
+  event.preventDefault();
+  modalOrder.classList.add("modal-order--modal-close");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    modalOrder.classList.add("modal-order--modal-close");
   }
 });
