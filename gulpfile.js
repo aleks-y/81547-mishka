5b5ac3fb3d83e4f7bba17db68209e0125485c00b
aleks-y <ustin89@mail.ru>
2016-10-06 10:33:24 +0300

    gulp builf fix

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,22 @@ var svgstore = require("gulp-svgstore")
 var server = require("browser-sync").create();
 var run = require("run-sequence");
 
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*{.woff,.woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+    ], {
+      base: "."
+    })
+    .pipe(gulp.dest("build"));
+});
+
 gulp.task("style", function() {
   gulp.src("less/style.less")
     .pipe(plumber())
@@ -26,10 +42,24 @@ gulp.task("style", function() {
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]})
+      ]}),
+      mqpacker({
+        sort: true
+      })
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("symbols", function() {
@@ -39,17 +69,28 @@ gulp.task("symbols", function() {
       inlineSvg: true
     }))
     .pipe(rename("symbols.svg"))
-    .pipe(gulp.dest("img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("serve", ["style"], function() {
   server.init({
-    server: ".",
+    server: "build",
     notify: false,
     open: true,
     ui: false
   });
 
-  gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("build/less/**/*.less", ["style"]);
+  gulp.watch("build/*.html").on("change", server.reload);
+});
+
+gulp.task("build", function(fn) {
+  run(
+      "clean",
+      "copy",
+      "style",
+      "images",
+      "symbols",
+      fn
+  );
 });
